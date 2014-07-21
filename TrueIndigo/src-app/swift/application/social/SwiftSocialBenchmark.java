039f5dbffb21e601613cc257e49b0222b49365ae
smd <smd@10-170-135-143.docente.di.fct.unl.pt>
2014-07-21 23:02:27 +0100

    Fixed snapshot computation to use current sequencer state.
    Fixed some typos

--- TrueIndigo/src-app/swift/application/social/SwiftSocialBenchmark.java
+++ TrueIndigo/src-app/swift/application/social/SwiftSocialBenchmark.java
@@ -46,129 +46,128 @@ import sys.utils.Threading;
  */
 public class SwiftSocialBenchmark extends SwiftSocialApp {
 
-    private static String shepard;
+	private static String shepard;
 
-    public void initDB(String[] args) {
+	public void initDB(String[] args) {
 
-        String propFile = Args.valueOf(args, "-props", "src-app/swift/application/social/swiftsocial-test.props");
-        Properties properties = Props.parseFile("swiftsocial", propFile);
+		String propFile = Args.valueOf(args, "-props", "src-app/swift/application/social/swiftsocial-test.props");
+		Properties properties = Props.parseFile("swiftsocial", propFile);
 
-        System.err.println("Populating db with users...");
+		System.err.println("Populating db with users...");
 
-        int numUsers = Props.intValue(properties, "swiftsocial.numUsers", 1000);
+		int numUsers = Props.intValue(properties, "swiftsocial.numUsers", 1000);
 
-        final int NumUsers = Args.valueOf(args, "-users", numUsers);
-        Workload.generateUsers(NumUsers);
+		final int NumUsers = Args.valueOf(args, "-users", numUsers);
+		Workload.generateUsers(NumUsers);
 
-        int threads = Args.valueOf(args, "-threads", 2);
-        final int PARTITION_SIZE = 1000;
-        int partitions = numUsers / PARTITION_SIZE + (numUsers % PARTITION_SIZE > 0 ? 1 : 0);
-        ExecutorService pool = Executors.newFixedThreadPool(threads);
+		int threads = Args.valueOf(args, "-threads", 2);
+		final int PARTITION_SIZE = 1000;
+		int partitions = numUsers / PARTITION_SIZE + (numUsers % PARTITION_SIZE > 0 ? 1 : 0);
+		ExecutorService pool = Executors.newFixedThreadPool(threads);
 
-        final AtomicInteger counter = new AtomicInteger(0);
-        for (int i = 0; i < partitions; i++) {
-            int lo = i * PARTITION_SIZE, hi = (i + 1) * PARTITION_SIZE;
-            final List<String> partition = Workload.getUserData().subList(lo, Math.min(hi, numUsers));
-            pool.execute(new Runnable() {
-                public void run() {
-                    SwiftSocialBenchmark.super.initUsers(partition, counter, NumUsers);
-                }
-            });
-        }
-        Threading.awaitTermination(pool, Integer.MAX_VALUE);
-        Threading.sleep(5000);
-        System.err.println("\nFinished populating db with users.");
-    }
+		final AtomicInteger counter = new AtomicInteger(0);
+		for (int i = 0; i < partitions; i++) {
+			int lo = i * PARTITION_SIZE, hi = (i + 1) * PARTITION_SIZE;
+			final List<String> partition = Workload.getUserData().subList(lo, Math.min(hi, numUsers));
+			pool.execute(new Runnable() {
+				public void run() {
+					SwiftSocialBenchmark.super.initUsers(partition, counter, NumUsers);
+				}
+			});
+		}
+		Threading.awaitTermination(pool, Integer.MAX_VALUE);
+		Threading.sleep(5000);
+		System.err.println("\nFinished populating db with users.");
+	}
 
-    public void doBenchmark(String[] args) {
-        super.init(args);
-        // IO.redirect("stdout.txt", "stderr.txt");
-        final long startTime = System.currentTimeMillis();
+	public void doBenchmark(String[] args) {
+		super.init(args);
+		// IO.redirect("stdout.txt", "stderr.txt");
+		final long startTime = System.currentTimeMillis();
 
-        System.err.println(IP.localHostname() + "/ starting...");
+		System.err.println(IP.localHostname() + "/ starting...");
 
-        int concurrentSessions = Args.valueOf(args, "-threads", 1);
+		int concurrentSessions = Args.valueOf(args, "-threads", 1);
 
-        String partitions = Args.valueOf(args, "-partition", "0/1");
-        int site = Integer.valueOf(partitions.split("/")[0]);
-        int numberOfSites = Integer.valueOf(partitions.split("/")[1]);
-        // ASSUMPTION: concurrentSessions is the same at all sites
-        int numberOfVirtualSites = numberOfSites * concurrentSessions;
+		String partitions = Args.valueOf(args, "-partition", "0/1");
+		int site = Integer.valueOf(partitions.split("/")[0]);
+		int numberOfSites = Integer.valueOf(partitions.split("/")[1]);
+		// ASSUMPTION: concurrentSessions is the same at all sites
+		int numberOfVirtualSites = numberOfSites * concurrentSessions;
 
-        shepard = Args.valueOf(args, "-shepard", "");
+		shepard = Args.valueOf(args, "-shepard", "");
 
-        System.err.println(IP.localHostAddress() + " connecting to: " + server);
+		System.err.println(IP.localHostAddress() + " connecting to: " + server);
 
-        super.populateWorkloadFromConfig();
+		super.populateWorkloadFromConfig();
 
-        SafeLog.printfComment("\n");
-        SafeLog.printfComment("\targs=%s\n", Arrays.asList(args));
-        SafeLog.printfComment("\tsite=%s\n", site);
-        SafeLog.printfComment("\tnumberOfSites=%s\n", numberOfSites);
-        SafeLog.printfComment("\tthreads=%s\n", concurrentSessions);
-        SafeLog.printfComment("\tnumberOfVirtualSites=%s\n", numberOfVirtualSites);
-        SafeLog.printfComment("\tSurrogate=%s\n", server);
-        SafeLog.printfComment("\tShepard=%s\n", shepard);
-        SafeLog.printHeader();
+		SafeLog.printfComment("\n");
+		SafeLog.printfComment("\targs=%s\n", Arrays.asList(args));
+		SafeLog.printfComment("\tsite=%s\n", site);
+		SafeLog.printfComment("\tnumberOfSites=%s\n", numberOfSites);
+		SafeLog.printfComment("\tthreads=%s\n", concurrentSessions);
+		SafeLog.printfComment("\tnumberOfVirtualSites=%s\n", numberOfVirtualSites);
+		SafeLog.printfComment("\tSurrogate=%s\n", server);
+		SafeLog.printfComment("\tShepard=%s\n", shepard);
+		SafeLog.printHeader();
 
-        if (!shepard.isEmpty())
-            Shepard.sheepJoinHerd(shepard);
+		if (!shepard.isEmpty())
+			Shepard.sheepJoinHerd(shepard);
 
-        // Kick off all sessions, throughput is limited by
-        // concurrentSessions.
-        final ExecutorService threadPool = Executors.newFixedThreadPool(concurrentSessions, Threading.factory("App"));
+		// Kick off all sessions, throughput is limited by
+		// concurrentSessions.
+		final ExecutorService threadPool = Executors.newFixedThreadPool(concurrentSessions, Threading.factory("App"));
 
-        System.err.println("Spawning session threads.");
-        for (int i = 0; i < concurrentSessions; i++) {
-            final int sessionId = site * concurrentSessions + i;
-            final Workload commands = getWorkloadFromConfig(sessionId, numberOfVirtualSites);
-            threadPool.execute(new Runnable() {
-                public void run() {
-                    // Randomize startup to avoid clients running all at the
-                    // same time; causes problems akin to DDOS symptoms.
-                    Threading.sleep(Sys.random().nextInt(1000));
-                    SwiftSocialBenchmark.super.runClientSession(Integer.toString(sessionId), commands, false);
-                }
-            });
-        }
+		System.err.println("Spawning session threads.");
+		for (int i = 0; i < concurrentSessions; i++) {
+			final int sessionId = site * concurrentSessions + i;
+			final Workload commands = getWorkloadFromConfig(sessionId, numberOfVirtualSites);
+			threadPool.execute(new Runnable() {
+				public void run() {
+					// Randomize startup to avoid clients running all at the
+					// same time; causes problems akin to DDOS symptoms.
+					Threading.sleep(Sys.random().nextInt(1000));
+					SwiftSocialBenchmark.super.runClientSession(Integer.toString(sessionId), commands, false);
+				}
+			});
+		}
 
-        Tasks.every(1.0, () -> {
-            System.err.printf("Done: %s\n", Progress.percentage(commandsDone.get(), totalCommands.get()));
-        });
+		Tasks.every(1.0, () -> {
+			System.err.printf("Done: %s\n", Progress.percentage(commandsDone.get(), totalCommands.get()));
+		});
 
-        // Wait for all sessions.
-        threadPool.shutdown();
-        Threading.awaitTermination(threadPool, Integer.MAX_VALUE);
+		// Wait for all sessions.
+		threadPool.shutdown();
+		Threading.awaitTermination(threadPool, Integer.MAX_VALUE);
 
-        System.err.println("Session threads completed.");
-        System.err.println("Throughput: " + totalCommands.get() * 1000 / (System.currentTimeMillis() - startTime)
-                + " txns/s");
-        System.exit(0);
-    }
+		System.err.println("Session threads completed.");
+		System.err.println("Throughput: " + totalCommands.get() * 1000 / (System.currentTimeMillis() - startTime) + " txns/s");
+		System.exit(0);
+	}
 
-    public static void main(String[] args) {
-        Args.use(args);
+	public static void main(String[] args) {
+		Args.use(args);
 
-        SwiftSocialBenchmark instance = new SwiftSocialBenchmark();
-        if (args.length == 0) {
+		SwiftSocialBenchmark instance = new SwiftSocialBenchmark();
+		if (args.length == 0) {
 
-            IndigoSequencerAndResourceManager.main(new String[] { "-name", "X" });
-            IndigoServer.main(new String[] { "-servers", "localhost" });
+			IndigoSequencerAndResourceManager.main(new String[]{"-name", "X"});
+			IndigoServer.main(new String[]{"-servers", "localhost"});
 
-            args = new String[] { "-server", "localhost", "-threads", "5", "-props", "swiftsocial-test.props" };
-            instance.initDB(args);
-            instance.doBenchmark(args);
-            exit(0);
-        }
+			args = new String[]{"-server", "localhost", "-threads", "5", "-props", "src-app/swift/application/social/swiftsocial-test.props"};
+			instance.initDB(args);
+			instance.doBenchmark(args);
+			exit(0);
+		}
 
-        if (Args.contains("init")) {
-            instance.initDB(args);
-            exit(0);
-        }
-        if (Args.contains("run")) {
-            instance.doBenchmark(args);
-            exit(0);
-        }
-        System.err.println("nothing to do...");
-    }
+		if (Args.contains("init")) {
+			instance.initDB(args);
+			exit(0);
+		}
+		if (Args.contains("run")) {
+			instance.doBenchmark(args);
+			exit(0);
+		}
+		System.err.println("nothing to do...");
+	}
 }
--- TrueIndigo/src/swift/indigo/StorageHelper.java
+++ TrueIndigo/src/swift/indigo/StorageHelper.java
@@ -33,171 +33,166 @@ import sys.net.api.Service;
 import sys.utils.Timings;
 
 public class StorageHelper {
-    private static Logger Log = Logger.getLogger(StorageHelper.class.getName());
+	private static Logger Log = Logger.getLogger(StorageHelper.class.getName());
 
-    final String LOCK_MANAGER;
-    final Service stub;
-    final Sequencer sequencer;
+	final String LOCK_MANAGER;
+	final Service stub;
+	final Sequencer sequencer;
 
-    _TxnHandle handle;
-    private final CausalityClock grantedRequests;
+	_TxnHandle handle;
+	private final CausalityClock grantedRequests;
 
-    private String resourceMgrId;
-    private IncrementalTimestampGenerator cltTimestampSource;
+	private String resourceMgrId;
+	private IncrementalTimestampGenerator cltTimestampSource;
 
-    private Map<Class, Class> tableToType;
+	private Map<Class, Class> tableToType;
 
-    private boolean isMasterLockManager;
+	private boolean isMasterLockManager;
 
-    final private Endpoint surrogate;
+	final private Endpoint surrogate;
 
-    // TODO: Sequencer should be remote
-    public StorageHelper(final Sequencer sequencer, String resourceMgrId, boolean isMasterLockManager) {
-        this.sequencer = sequencer;
-        this.LOCK_MANAGER = sequencer.siteId + "LockManager";
-        this.stub = sequencer.stub;
-        this.isMasterLockManager = isMasterLockManager;
-        this.surrogate = Networking.resolve(Defaults.SERVER_URL);
+	// TODO: Sequencer should be remote
+	public StorageHelper(final Sequencer sequencer, String resourceMgrId, boolean isMasterLockManager) {
+		this.sequencer = sequencer;
+		this.LOCK_MANAGER = sequencer.siteId + "LockManager";
+		this.stub = sequencer.stub;
+		this.isMasterLockManager = isMasterLockManager;
+		this.surrogate = Networking.resolve(Defaults.SERVER_URL);
 
-        this.grantedRequests = sequencer.clocks.currentClockCopy();
-        this.cltTimestampSource = new IncrementalTimestampGenerator(resourceMgrId, 0L);
-        this.tableToType = new HashMap<Class, Class>();
-    }
+		this.grantedRequests = sequencer.clocks.currentClockCopy();
+		this.cltTimestampSource = new IncrementalTimestampGenerator(resourceMgrId, 0L);
+		this.tableToType = new HashMap<Class, Class>();
+	}
 
-    public <A extends ResourceRequest<?>, B extends CRDT<?>> void registerType(Class<A> requestType, Class<B> theClass) {
-        this.tableToType.put(requestType, theClass);
-    }
+	public <A extends ResourceRequest<?>, B extends CRDT<?>> void registerType(Class<A> requestType, Class<B> theClass) {
+		this.tableToType.put(requestType, theClass);
+	}
 
-    synchronized CausalityClock getCurrentClock() {
-        grantedRequests.merge(sequencer.clocks.currentClockCopy());
-        return grantedRequests.clone();
-    }
+	synchronized CausalityClock getCurrentClock() {
+		return sequencer.clocks.currentClockCopy();
+	}
 
-    void beginTxn(Timestamp cltTimestamp) {
-        handle = new _TxnHandle(getCurrentClock(), cltTimestamp);
-    }
+	void beginTxn(Timestamp cltTimestamp) {
+		handle = new _TxnHandle(getCurrentClock(), cltTimestamp);
+	}
 
-    public void endTxn(final boolean writeThrough) {
-        if (writeThrough)
-            handle.commit();
-    }
+	public void endTxn(final boolean writeThrough) {
+		if (writeThrough)
+			handle.commit();
+	}
 
-    public Resource getResource(ResourceRequest<?> req) throws SwiftException {
-        Class<CRDT> type = tableToType.get(req.getClass());
-        Resource resource = (Resource<?>) handle.getMostRecent(req.getResourceId(), isMasterLockManager, type);
-        return resource;
-    }
+	public Resource getResource(ResourceRequest<?> req) throws SwiftException {
+		Class<CRDT> type = tableToType.get(req.getClass());
+		Resource resource = (Resource<?>) handle.getMostRecent(req.getResourceId(), isMasterLockManager, type);
+		return resource;
+	}
 
-    // ATTENTION: Original code used locks to protect the creation of the lock
-    public void createResources(Collection<ResourceRequest<?>> request) throws SwiftException {
-        for (ResourceRequest<?> req : request) {
-            createResource(req);
-        }
-    }
+	// ATTENTION: Original code used locks to protect the creation of the lock
+	public void createResources(Collection<ResourceRequest<?>> request) throws SwiftException {
+		for (ResourceRequest<?> req : request) {
+			createResource(req);
+		}
+	}
 
-    public Resource<?> createResource(ResourceRequest<?> req) throws SwiftException {
-        if (Log.isLoggable(Level.INFO))
-            Log.info("Created resource for request " + req);
-        Class<CRDT> type = tableToType.get(req.getResourceId().getTable());
-        Resource resource = (Resource) handle.getMostRecent(req.getResourceId(), true, type);
-        resource.initialize(sequencer.siteId, req);
-        return resource;
-    }
+	public Resource<?> createResource(ResourceRequest<?> req) throws SwiftException {
+		if (Log.isLoggable(Level.INFO))
+			Log.info("Created resource for request " + req);
+		Class<CRDT> type = tableToType.get(req.getResourceId().getTable());
+		Resource resource = (Resource) handle.getMostRecent(req.getResourceId(), true, type);
+		resource.initialize(sequencer.siteId, req);
+		return resource;
+	}
 
-    class _TxnHandle extends AbstractTxHandle {
+	class _TxnHandle extends AbstractTxHandle {
 
-        _TxnHandle(CausalityClock snapshot, Timestamp cltTimestamp) {
-            super(snapshot, cltTimestamp);
-        }
+		_TxnHandle(CausalityClock snapshot, Timestamp cltTimestamp) {
+			super(snapshot, cltTimestamp);
+		}
 
-        @Override
-        public void commit() {
-            try {
-                Timings.mark();
-                final CommitUpdatesRequest req;
-                List<CRDTObjectUpdatesGroup<?>> updates = getUpdates();
+		@Override
+		public void commit() {
+			try {
+				Timings.mark();
+				final CommitUpdatesRequest req;
+				List<CRDTObjectUpdatesGroup<?>> updates = getUpdates();
 
-                if (!updates.isEmpty()) {
-                    Timestamp ts = sequencer.clocks.newTimestamp();
+				if (!updates.isEmpty()) {
+					Timestamp ts = sequencer.clocks.newTimestamp();
 
-                    req = new CommitUpdatesRequest(LOCK_MANAGER + "-" + sequencer.siteId, cltTimestamp(), snapshot,
-                            updates);
-                    req.setTimestamp(ts);
-                } else {
-                    req = new CommitUpdatesRequest(LOCK_MANAGER + "-" + sequencer.siteId, new Timestamp("dummy", -1L),
-                            snapshot, updates);
-                }
+					req = new CommitUpdatesRequest(LOCK_MANAGER + "-" + sequencer.siteId, cltTimestamp(), snapshot, updates);
+					req.setTimestamp(ts);
+				} else {
+					req = new CommitUpdatesRequest(LOCK_MANAGER + "-" + sequencer.siteId, new Timestamp("dummy", -1L), snapshot, updates);
+				}
 
-                final Semaphore semaphore = new Semaphore(0);
-                stub.asyncRequest(surrogate, req, (CommitUpdatesReply r) -> {
-                    if (Log.isLoggable(Level.INFO))
-                        Log.info("FINISH COMMIT------->>" + r.getStatus() + " FOR : " + req.getTimestamp());
-                    semaphore.release();
-                });
-                semaphore.acquireUninterruptibly();
-            } finally {
-                Timings.sample("async commit");
-            }
-        }
+				final Semaphore semaphore = new Semaphore(0);
+				stub.asyncRequest(surrogate, req, (CommitUpdatesReply r) -> {
+					if (Log.isLoggable(Level.INFO))
+						Log.info("FINISH COMMIT------->>" + r.getStatus() + " FOR : " + req.getTimestamp());
+					semaphore.release();
+				});
+				semaphore.acquireUninterruptibly();
+			} finally {
+				Timings.sample("async commit");
+			}
+		}
 
-        @SuppressWarnings("unchecked")
-        @Override
-        protected <V extends CRDT<V>> ManagedCRDT<V> getCRDT(CRDTIdentifier uid, CausalityClock version,
-                boolean create, Class<V> classOfV) throws VersionNotFoundException {
-            try {
-                Timings.mark();
-                FetchObjectVersionRequest req = new FetchObjectVersionRequest(LOCK_MANAGER, uid, version, true);
+		@SuppressWarnings("unchecked")
+		@Override
+		protected <V extends CRDT<V>> ManagedCRDT<V> getCRDT(CRDTIdentifier uid, CausalityClock version, boolean create, Class<V> classOfV) throws VersionNotFoundException {
+			try {
+				Timings.mark();
+				FetchObjectVersionRequest req = new FetchObjectVersionRequest(LOCK_MANAGER, uid, version, true);
 
-                Timings.mark();
-                FetchObjectVersionReply reply = stub.request(surrogate, req);
-                Timings.sample("fetchCRDT(" + uid + ") from" + surrogate);
+				Timings.mark();
+				FetchObjectVersionReply reply = stub.request(surrogate, req);
+				Timings.sample("fetchCRDT(" + uid + ") from" + surrogate);
 
-                if (reply != null) {
-                    if (reply.getStatus() == FetchObjectVersionReply.FetchStatus.OK) {
-                        return (ManagedCRDT<V>) reply.getCrdt();
-                    }
-                    if (create && reply.getStatus() == FetchObjectVersionReply.FetchStatus.OBJECT_NOT_FOUND) {
-                        return createCRDT(uid, version, classOfV);
-                    }
-                    if (reply.getStatus() == FetchObjectVersionReply.FetchStatus.VERSION_NOT_FOUND) {
-                        throw new VersionNotFoundException("Version nout found");
-                    }
-                }
-                return null;
-            } finally {
-                Timings.sample("getCRDT(" + uid + ")");
-            }
-        }
+				if (reply != null) {
+					if (reply.getStatus() == FetchObjectVersionReply.FetchStatus.OK) {
+						return (ManagedCRDT<V>) reply.getCrdt();
+					}
+					if (create && reply.getStatus() == FetchObjectVersionReply.FetchStatus.OBJECT_NOT_FOUND) {
+						return createCRDT(uid, version, classOfV);
+					}
+					if (reply.getStatus() == FetchObjectVersionReply.FetchStatus.VERSION_NOT_FOUND) {
+						throw new VersionNotFoundException("Version nout found");
+					}
+				}
+				return null;
+			} finally {
+				Timings.sample("getCRDT(" + uid + ")");
+			}
+		}
 
-        public <V extends CRDT<V>> V getMostRecent(CRDTIdentifier id, boolean create, Class<V> classOfV)
-                throws WrongTypeException, NoSuchObjectException, VersionNotFoundException, NetworkException {
+		public <V extends CRDT<V>> V getMostRecent(CRDTIdentifier id, boolean create, Class<V> classOfV) throws WrongTypeException, NoSuchObjectException, VersionNotFoundException, NetworkException {
 
-            return (V) this.getCRDT(id, getCurrentClock(), create, classOfV).getLatestVersion(this);
-        }
+			return (V) this.getCRDT(id, getCurrentClock(), create, classOfV).getLatestVersion(this);
+		}
 
-        protected Timestamp cltTimestamp() {
-            if (cltTimestamp == null)
-                cltTimestamp = cltTimestampSource.generateNew();
+		protected Timestamp cltTimestamp() {
+			if (cltTimestamp == null)
+				cltTimestamp = cltTimestampSource.generateNew();
 
-            return cltTimestamp;
-        }
-    }
+			return cltTimestamp;
+		}
+	}
 
-    public Timestamp recordNewEvent() {
-        Timestamp ts = sequencer.clocks.newTimestamp(); // short circuit
-        // sequencer, is it
-        // safe????
-        if (ts != null)
-            synchronized (grantedRequests) {
-                grantedRequests.record(ts);
-            }
-        return ts;
-    }
+	public Timestamp recordNewEvent() {
+		Timestamp ts = sequencer.clocks.newTimestamp(); // short circuit
+		// sequencer, is it
+		// safe????
+		if (ts != null)
+			synchronized (grantedRequests) {
+				grantedRequests.record(ts);
+			}
+		return ts;
+	}
 
-    public Collection<CRDTObjectUpdatesGroup<?>> endTxnAndGetUpdates(boolean b) {
-        endTxn(b);
-        return handle.getUpdates();
+	public Collection<CRDTObjectUpdatesGroup<?>> endTxnAndGetUpdates(boolean b) {
+		endTxn(b);
+		return handle.getUpdates();
 
-    }
+	}
 
 }
